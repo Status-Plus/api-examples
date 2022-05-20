487312435b2f22907f7b476c2452e5d4d718404a
User <65233836+User9684@users.noreply.github.com>
2022-05-20 16:43:34 -0500

    Update source.py

--- languages/python/asciitablestatus/source.py
+++ languages/python/asciitablestatus/source.py
@@ -3,12 +3,12 @@ import json
 import requests
 from prettytable import PrettyTable
 
-req = requests.get('https://api.statusplus.xyz/status')
-statuses = req.json()
-newTable = PrettyTable()
+req = requests.get('https://api.statusplus.xyz/status');
+statuses = req.json();
+newTable = PrettyTable();
 newTable.field_names = ['API Name','Status','Response Time (ms)','HTTP Code']
 
 for api in statuses:
-    newTable.add_row([api['name'],api['status'],api['response_time'],api['response_code']])
+    newTable.add_row([api['name'],api['status'],api['response_time'],api['response_code']]);
 
-print(newTable)
+print(newTable);
